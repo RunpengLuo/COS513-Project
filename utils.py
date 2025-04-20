e5a1002192045213e133deca4b9335d9c9718486
RunpengLuo <runpeng.luo@princeton.edu>
2025-04-20 15:23:18 -0400

    load dataset json file

---
+++ utils.py
@@ -0,0 +1,15 @@
+import json
+import pandas as pd
+
+def json_to_df(filepath: str):
+    """
+    convert json to pandas dataframe
+    """
+    data_file = open(filepath)
+    data = []
+    for line in data_file:
+        data.append(json.loads(line))
+    data_file.close()
+
+    assert len(data) > 0, "json file is empty"
+    return pd.DataFrame(data)
